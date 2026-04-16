3d8b96aa57c385758676e9e3253f3bbdc0b33e78
ywisteria03150419-beep <ywisteria03150419@gmail.com>
2026-04-16 20:33:12 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+// おけいこ日記 Service Worker
+const CACHE_NAME = 'okeiko-diary-v1';
+const URLS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE.filter(url => !url.includes('icon'))))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
